81b0d014fbbbb8d1d55446fa8b619ebe2751ce3f
MT 07 <tanlalan123@gmail.com>
2020-12-22 19:40:06 +0700

    Trajectory

---
+++ Assets/Scripts/Map Generator/MapGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    Camera cam;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+    //Letter box
+    private void Start()
+    {
+        Vector3 leftPoint = cam.MiddleLeftPoint();
+        Vector3 rightPoint = cam.MiddleRightPoint();
+
+        Debug.Log(Vector3.Distance(leftPoint,rightPoint));
+        Debug.Log(rightPoint);
+
+    }
+
+}
