a55cd67df5997e8b75b381361670b1eee2949413
Bich0107 <hoangbichht@gmail.com>
2020-12-23 17:08:21 +0700

    Update (Bich): Draw ball trajectory (dot line, collide screen only), add helper files (MonoSingleton, ObjectPool, CollideChecker), dot marterial and some small changes.

---
+++ Assets/Scripts/Helper/ObjectPool.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Preallocation
+{
+    public GameObject gameObject;
+    public int count;
+    public bool expandable;
+}
+
+public class ObjectPool : MonoSingleton<ObjectPool>
+{
+    public List<Preallocation> preAllocations;
+
+    [SerializeField]
+    List<GameObject> pooledGobjects;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        pooledGobjects = new List<GameObject>();
+
+        foreach (Preallocation item in preAllocations)
+        {
+            for (int i = 0; i < item.count; ++i)
+            {
+                pooledGobjects.Add(CreateGobject(item.gameObject));
+            }
+        }
+    }
+
+    public GameObject Spawn(string tag)
+    {
+        for (int i = 0; i < pooledGobjects.Count; ++i)
+        {
+            if (!pooledGobjects[i].activeSelf && pooledGobjects[i].tag.Equals(tag))
+            {
+                return pooledGobjects[i];
+            }
+        }
+
+        for (int i = 0; i < preAllocations.Count; ++i)
+        {
+            if (pooledGobjects[i].gameObject.tag.Equals(tag))
+                if (preAllocations[i].expandable)
+                {
+                    GameObject obj = CreateGobject(preAllocations[i].gameObject);
+                    pooledGobjects.Add(obj);
+                    return obj;
+                }
+        }
+        return null;
+    }
+
+    GameObject CreateGobject(GameObject item)
+    {
+        GameObject gobject = Instantiate(item, transform);
+        gobject.SetActive(false);
+        return gobject;
+    }
+
+    public void Reset()
+    {
+        for (int i = 0; i < pooledGobjects.Count; i++)
+        {
+            pooledGobjects[i].SetActive(false);
+        }
+    }
+}
