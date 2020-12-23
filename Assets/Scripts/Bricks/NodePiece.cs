b11830366b3d2463228204ef47809a315343bee1
MT 07 <tanlalan123@gmail.com>
2020-12-23 21:07:11 +0700

    Add text to bricks

---
+++ Assets/Scripts/Bricks/NodePiece.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class NodePiece : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _textPoint;
+    [SerializeField] int _point;
+
+    public int Point
+    {
+        get => _point;
+        set
+        {
+            _point = value;
+            _textPoint.text = _point.ToString();
+        }
+    }
+
+    private void Awake()
+    {
+        Point = _point;
+    }
+}
--- Assets/Scripts/Map Generator/MapGenerator.cs
+++ Assets/Scripts/Map Generator/MapGenerator.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     Camera cam;
-    [SerializeField] GameObject test;
+    [SerializeField] GameObject brick;
     [SerializeField] int numOfWidth;
     [SerializeField] float offset;
     public ArrayInt[] mArray;
@@ -19,27 +19,51 @@ public class MapGenerator : MonoBehaviour
     //Letter box
     private void Start()
     {
-        
-        Debug.Log(mArray[mArray.Length - 1][mArray[mArray.Length - 1].Length - 1]); //debugs the last value
+
+        DrawMatrix();
+    }
 
 
+    public void DrawMatrix()
+    {
         Vector2 leftPoint = cam.TopLeftPoint();
         Vector2 rightPoint = cam.TopRightPoint();
-        float distance = (Vector2.Distance(leftPoint, rightPoint) - offset * (numOfWidth - 1)) / numOfWidth;
+        float distance = (Vector2.Distance(leftPoint, rightPoint) - offset * (numOfWidth - 1)) / numOfWidth;  //Distance mean scale 
+        float scaleFactor = brick.GetComponent<SpriteRenderer>().sprite.rect.width / brick.GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
 
+        //Convert scale to distance accurate
+        distance /= scaleFactor;
 
-        for (int i = 0; i < 9; i++)
+        for (int i = 0; i < numOfWidth; i++)
         {
-            Vector2 fistPos = new Vector2(leftPoint.x + distance / 2, leftPoint.y - distance / 2);
-            for (int j = 0; j < 9; j++)
+            Vector2 fistPos = new Vector2(leftPoint.x, leftPoint.y);
+            float distanceY = 0f;
+            for (int j = 0; j < numOfWidth; j++)
             {
-                GameObject block = Instantiate(test);
-                block.transform.localScale = new Vector2(distance, distance);
-                block.transform.position = new Vector2(fistPos.x, fistPos.y);
-                fistPos = new Vector2(fistPos.x + distance + offset, fistPos.y);
+                if (mArray[i][j] == 0)
+                {
+
+                }
+                else if (mArray[i][j] == -1)
+                {
+
+                }
+                else
+                {
+                    GameObject block = Instantiate(brick, transform);
+                    //Setup
+                    block.transform.localScale = new Vector2(distance, distance);
+                    block.transform.position = new Vector2(fistPos.x + block.GetComponent<SpriteRenderer>().bounds.size.x / 2, fistPos.y - block.GetComponent<SpriteRenderer>().bounds.size.y / 2);
+                    fistPos = new Vector2(fistPos.x + block.GetComponent<SpriteRenderer>().bounds.size.x + offset, fistPos.y);
+                    block.GetComponent<Brick>().Point = mArray[i][j];
+
+                    //Get distance Heigh
+                    distanceY = block.GetComponent<SpriteRenderer>().bounds.size.y;
+                }
             }
-            leftPoint = new Vector2(leftPoint.x, leftPoint.y - distance - offset);
+            leftPoint = new Vector2(leftPoint.x, leftPoint.y - distanceY - offset);
         }
+
     }
 
 }
